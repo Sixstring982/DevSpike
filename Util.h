39cbdb8aa471820ad9d3208179ae053a5a7dbd9b
Trent Small <Sixstring982@gmail.com>
2014-09-24 12:30:36 -0600

    Distance2D Macro

--- Util.h
+++ Util.h
@@ -1,10 +1,13 @@
 #ifndef UTIL_H
 #define UTIL_H
 
+#include <math.h>
+
 #define MIN(X,Y) ((X) > (Y) ? (Y) : (X))
 #define MAX(X,Y) ((X) < (Y) ? (Y) : (X))
 
 #define ABS(X) ((X) > 0 ? (X) : -(X))
+#define DIST2D(X, Y) sqrt((X) * (X) + (Y) * (Y))
 
 typedef enum {false, true} bool;
 
