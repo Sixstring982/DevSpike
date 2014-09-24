fe8d80a9d54db08268c27bd3aa3063c6593ebb01
Trent Small <Sixstring982@gmail.com>
2014-09-24 12:08:22 -0600

    Util needs an ABS macro

--- Util.h
+++ Util.h
@@ -4,6 +4,8 @@
 #define MIN(X,Y) ((X) > (Y) ? (Y) : (X))
 #define MAX(X,Y) ((X) < (Y) ? (Y) : (X))
 
+#define ABS(X) ((X) > 0 ? (X) : -(X))
+
 typedef enum {false, true} bool;
 
 #endif /* UTIL_H */
